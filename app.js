dfc1dfe9bc43760f97dc84f4a16e7301a671b669
betul.senger@gkgtechnology.com <betul.senger@gkgtechnology.com>
2022-05-08 16:24:35 +0300

    face detection from photo

--- app.js
+++ app.js
@@ -1,48 +1,84 @@
-const video = document.getElementById("video");
+const myImage = document.getElementById("image");
+const localHost = "http://127.0.0.1:5500";
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models")
-]).then(startCamera());
+  // faceapi.nets.faceRecognitionNet.loadFromUri(localHost + "/models")
+  faceapi.nets.faceRecognitionNet.loadFromUri(`${localHost}/models`),
+  faceapi.nets.faceLandmark68Net.loadFromUri(`${localHost}/models`),
+  faceapi.nets.ssdMobilenetv1.loadFromUri(`${localHost}/models`)
+]).then(start);
 
-function startCamera() {
-  //   return false;
-  navigator.getUserMedia(
-    {
-      video: {}
-    },
-    stream => (video.srcObject = stream),
-    err => console.log(err)
-  );
+async function start() {
+  document.body.append("Tüm modeller Yüklendi..");
+
+  myImage.addEventListener("change", async () => {
+    const selectedImage = await faceapi.bufferToImage(myImage.files[0]);
+    document.body.append(selectedImage);
+
+    const detections = await faceapi
+      .detectAllFaces(selectedImage)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+
+    const displaySize = {
+      width: selectedImage.width,
+      height: selectedImage.height
+    };
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    console.log(resizedDetections);
+
+    const canvas = faceapi.createCanvasFromMedia(selectedImage);
+    document.body.append(canvas);
+    faceapi.matchDimensions(canvas, displaySize);
+
+    // resizedDetections.forEach(d => {
+    //   const box = d.detection.box;
+    //   const drawBox = new faceapi.draw.DrawBox(box, { label: "Kimdir Bu?" });
+    //   drawBox.draw(canvas);
+    // });
+
+    const results = resizedDetections.map(d =>
+      faceMatcher.findBestMatch(d.descriptor)
+    );
+
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: result.toString()
+      });
+      drawBox.draw(canvas);
+    });
+  });
+
+  const labelDescriptions = await loadImages();
+  const faceMatcher = new faceapi.FaceMatcher(labelDescriptions, 0.6);
 }
 
-video.addEventListener("play", () => {
-  const canvas = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvas);
-  const boxSize = {
-    width: video.width,
-    height: video.height
-  };
+async function loadImages() {
+  const labels = [
+    "angelina",
+    "betul",
+    "brad",
+    "johny"
+  ];
 
-  faceapi.matchDimensions(canvas, boxSize);
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = [];
 
-  setInterval(async () => {
-    const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceExpressions();
+      for (let i = 1; i <= 3; i++) {
+        const image = await faceapi.fetchImage(
+          `${localHost}/library/${label}/${i}.jpg`
+        );
 
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    const resizedDetections = faceapi.resizeResults(detections, boxSize);
-
-    faceapi.draw.drawDetections(canvas, resizedDetections);
-
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-
-    // console.log(detections);
-  }, 100);
-});
+        const detections = await faceapi
+          .detectSingleFace(image)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
+}
